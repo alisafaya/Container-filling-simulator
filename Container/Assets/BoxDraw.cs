7f99029469f687fa098b2cbf00dac19debeb7f7d
AliSafaya <alisafaya@gmail.com>
2016-12-12 19:42:06 +0300

    10 yillik odev

---
+++ Container/Assets/BoxDraw.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+public class BoxDraw : MonoBehaviour {
+
+    static List<Kutu> kutular = new List<Kutu>();
+
+    static List<Kutu> sonuc_sira = new List<Kutu>();
+    void Start () {
+        System.Random rnd = new System.Random();
+
+        for (int i = 1; i < 1000; i++)
+        {
+            int j = rnd.Next(1, 10);
+
+            int en = rnd.Next(20, 30);
+            int boy = rnd.Next(10, 40);
+            int derinlik = rnd.Next(15, 40);
+
+            for (int a = 0; a < j; a++)
+            {
+                kutular.Add(new Kutu(en, boy, derinlik, false));
+            }
+        }
+        
+
+        
+        Debug.Log("kutular sayisi: " + kutular.Count);
+        Kutu katman = new Kutu(234, 234, 1200, true);
+        katman.konumAyarla(0, 0, 0);
+        GameObject konteyner = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        konteyner.transform.position = new Vector3(katman.y + (katman.en / 2), katman.z + (katman.boy / 2), katman.x + (katman.derinlik / 2));
+        konteyner.transform.localScale = new Vector3(katman.en, katman.boy, katman.derinlik);
+        yerlestir(katman);
+
+        List<Kutu> hatalar = new List<Kutu>();
+
+        foreach (var item1 in sonuc_sira)
+        {
+            foreach (var item2 in sonuc_sira)
+            {
+
+                if (sonuc_sira.IndexOf(item1) != sonuc_sira.IndexOf(item2) && item1.x == item2.x && item1.y == item2.y && item1.z == item2.z)
+                {
+                    hatalar.Add(item2);
+                }
+
+            }
+        }
+
+        foreach (var item in hatalar)
+        {
+            sonuc_sira.Remove(item);
+        }
+
+        foreach (var item in sonuc_sira)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(item.y + (item.en /2), item.z + (item.boy/2), item.x + (item.derinlik/2));
+            cube.transform.localScale = new Vector3(item.en, item.boy, item.derinlik);
+           // cube.GetComponent<Material>().color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value, 1.0f);
+        }
+        Debug.Log("Sonuc Sayisi :" + sonuc_sira.Count);
+        Debug.Log("kutu hacmi :" + sonuc_sira.Sum(m => m.hacim));
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    
+    static void yerlestir(Kutu katman)
+    {
+        //Debug.Log("Katman ozellikleri" + katman.derinlik + " " + katman.en + " " + katman.boy + " " + katman.x + " " + katman.y + " " + katman.z);
+
+        kutular = kutular.OrderByDescending(x => x.hacim).ToList();
+        float blokDerinlik = 0f;
+        if (kutular.Count == 0)
+        {
+            return;
+        }
+        Kutu referans = null;
+        List<Kutu> blok = new List<Kutu>();
+
+        for (int i = 0; i < kutular.Count; i++)
+        {
+            referans = kutular[i];
+            if (referans.boy < katman.boy && referans.derinlik < katman.derinlik && referans.en < katman.en)
+            {
+                blok.Add(referans);
+                blokDerinlik += referans.derinlik;
+                kutular.Remove(referans);
+                break;
+            }
+            else
+            {
+                referans = null;
+            }
+
+        }
+
+        if (referans == null)
+        {
+            return;
+        }
+
+        referans.konumAyarla(katman.x, katman.y, katman.z);
+
+        for (int i = 1; i < kutular.Count; i++)
+        {
+
+            if (referans.en == kutular[i].en && referans.boy == kutular[i].boy)
+            {
+
+                if ((blokDerinlik + kutular[i].derinlik) > katman.derinlik)
+                {
+                    break;
+                }
+
+                kutular[i].konumAyarla((blok[blok.Count - 1].x + blok[blok.Count - 1].derinlik), blok[blok.Count - 1].y, blok[blok.Count - 1].z);
+
+
+                blok.Add(kutular[i]);
+                blokDerinlik += kutular[i].derinlik;
+                kutular.Remove(kutular[i]);
+
+            }
+
+        }
+
+        foreach (var item in blok)
+        {
+            sonuc_sira.Add(item);
+        }
+
+        Kutu katmanyan = new Kutu(katman.en - referans.en, (referans.boy), blokDerinlik, true);
+        katmanyan.konumAyarla(katman.x, (referans.en + katman.y), katman.z);
+
+        Kutu katmanon = new Kutu(katman.en, katman.boy, (katman.derinlik - blokDerinlik), true);
+        katmanon.konumAyarla((blokDerinlik + katman.x), katman.y, katman.z);
+
+        Kutu katmanust = new Kutu(katman.en, (katman.boy - referans.boy), blokDerinlik, true);
+        katmanust.konumAyarla(katman.x, katman.y, (katman.z + referans.boy));
+
+
+        yerlestir(katmanust);
+        yerlestir(katmanon);
+        yerlestir(katmanyan);
+
+    }
+
+}
---
+++ Container/Assets/Kutu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+class Kutu
+{
+    public float en { get; set; }
+    public float boy { get; set; }
+    public float derinlik { get; set; }
+    public float x { get; set; }
+    public float y { get; set; }
+    public float z { get; set; }
+    public float hacim { get; set; }
+
+    public Kutu(float en, float boy, float derinlik, bool katmanmi)
+    {
+       // float tolerans = katmanmi == true ? 0 : -2;
+        if (!katmanmi)
+        {
+            if (en > derinlik)
+            {
+                float gecici = derinlik;
+                derinlik = en;
+                en = gecici;
+            }
+            if (boy > derinlik)
+            {
+                float gecici = derinlik;
+                derinlik = boy;
+                boy = gecici;
+            }
+            if (en > boy)
+            {
+                float gecici = en;
+                en = boy;
+                boy = gecici;
+            } 
+        }
+        this.hacim = en * boy * derinlik;
+        this.en = en;
+        this.boy = boy;
+        this.derinlik = derinlik;
+
+    }
+    public void konumAyarla(float _x, float _y, float _z)
+    {
+        this.x = _x;        
+        this.y = _y ;
+        this.z = _z ;
+    }
+}
